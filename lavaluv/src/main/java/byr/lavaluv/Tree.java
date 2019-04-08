d26723cffc90eab27638a880fa3c793bf0a0f06e
lavaluv <771992043@qq.com>
2019-04-08 14:05:30 +0800

    add traverseBydepth

--- lavaluv/src/main/java/byr/lavaluv/Tree.java
+++ lavaluv/src/main/java/byr/lavaluv/Tree.java
@@ -1,5 +1,8 @@
 package byr.lavaluv;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Tree<T> {
 	private T dataT;
 	private Node<Tree<T>> treeList;
@@ -32,6 +35,14 @@ public class Tree<T> {
 			return false;
 		}
 	}
+	public boolean addTree(Tree<T> tree) {
+		try {
+			return this.treeList.addData(tree);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 	public Tree<T> getSubTree(int index){
 		try {
 			return this.treeList.findDataByIndex(index);
@@ -40,45 +51,82 @@ public class Tree<T> {
 			return null;
 		}
 	}
-	public int getDepth(Tree<T> tree,int root) {
-		Node<Tree<T>> node = tree.treeList.nextNode();
-		int max = root;
-		while (node != null) {
-			if (node.getData() != null) {
-				root ++;
-				int subDepth = getDepth(node.getData(),root);
-				if (max < subDepth) {
-					max = subDepth;
-				}
-				root--;
-			}
-			node = node.nextNode();
+	public boolean deleteTree(int index) {
+		try {
+			return this.treeList.deleteNode(index);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
 		}
-		return max;
 	}
-	public void traverseBylevel(Tree<T> tree) {
-		System.out.println("Level :"+tree.getData());
+	@SuppressWarnings("unchecked")
+	public int getDepth() {
+		int depth = 0;
+		ArrayList<Tree<T>> tList = new ArrayList<Tree<T>>();
+		ArrayList<Tree<T>> tempArrayList = new ArrayList<Tree<T>>();
+		tList.add(this);
+		Iterator<Tree<T>> iterator;
+		while (!tList.isEmpty()) {
+			depth++;
+			iterator = tList.iterator();
+			while (iterator.hasNext()) {
+				Tree<T> tree = iterator.next();
+				Node<Tree<T>> node = tree.treeList.nextNode();
+				while (node != null) {
+					tempArrayList.add(node.getData());
+					node = node.nextNode();
+				}
+			}
+			tList.clear();
+			tList = (ArrayList<Tree<T>>) tempArrayList.clone();
+			tempArrayList.clear();
+		}
+		return depth;
+	}
+	@SuppressWarnings("unchecked")
+	public void traverseBylevel() {
+		ArrayList<Tree<T>> tList = new ArrayList<Tree<T>>();
+		ArrayList<Tree<T>> tempArrayList = new ArrayList<Tree<T>>();
+		tList.add(this);
+		Iterator<Tree<T>> iterator;
+		while (!tList.isEmpty()) {
+			iterator = tList.iterator();
+			System.out.print("(");
+			while (iterator.hasNext()) {
+				Tree<T> tree = iterator.next();
+				Node<Tree<T>> node = tree.treeList.nextNode();
+				System.out.print(tree.getData());
+				while (node != null) {
+					tempArrayList.add(node.getData());
+					node = node.nextNode();
+				}
+			}
+			System.out.print(")");
+			tList.clear();
+			tList = (ArrayList<Tree<T>>) tempArrayList.clone();
+			tempArrayList.clear();
+		}
+	}
+	//先序遍历
+	public void traverseByDepth() {
+		System.out.print("("+this.getData());
 		Node<Tree<T>> node = this.treeList.nextNode();
 		while (node != null) {
-			if (node.getData() != null) {
-				System.out.println(node.getData().getData());
-			}
+			node.getData().traverseByDepth();
 			node = node.nextNode();
 		}
-		node = this.treeList.nextNode();
-		while (node != null) {
-			if (node.getData() != null) {
-				System.out.println("ok");	
-			}
-			node = node.nextNode();
-		}
+		System.out.print(")");
 	}
 	public static void main(String args[])throws Exception{
 		Tree<Integer> teTree = new Tree<>(1);
 		teTree.addBranch(teTree, 2);
 		teTree.addBranch(teTree, 3);
-		teTree.addBranch(teTree.getSubTree(1), 4);
-		System.out.println(teTree.getDepth(teTree,1));
-		teTree.traverseBylevel(teTree);
+		teTree.addBranch(teTree.getSubTree(0), 4);
+		//teTree.deleteTree(1);
+		Tree<Integer> newTree = new Tree<Integer>(5);
+		newTree.addBranch(newTree, 6);
+		teTree.addTree(newTree);
+		System.out.println(teTree.getDepth());
+		teTree.traverseByDepth();
 	}
 }
